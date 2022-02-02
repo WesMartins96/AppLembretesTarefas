f1bbaaddf1dd42c68fb0489cffad0069a5e32f81
Wesley Martins <wesmartins96@gmail.com>
2022-02-02 02:14:40 -0300

    Projeto Finalizado com Sucesso

--- app/src/main/java/com/example/applembretestarefas/ui/MainActivity.kt
+++ app/src/main/java/com/example/applembretestarefas/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.applembretestarefas.databinding.ActivityMainBinding
 import com.example.applembretestarefas.datasource.TaskDataSource
 
@@ -13,10 +14,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter by lazy { ListAdapterTask() }
 
+    /**
+     * Nova maneira de iniciar uma activity.
+     * Já que `startActivityForResult` foi depreciado.
+     */
+
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) updateList()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
 
         updateList()
         insertListeners()
@@ -25,13 +38,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun insertListeners() {
         binding.btnAddTasks.setOnClickListener {
-            startActivityForResult(Intent(this, AddActivity::class.java), CREATE_NEW_TASK)
+            register.launch(Intent(this, AddActivity::class.java))
         }
 
         adapter.listenerEdit = {
             val intent = Intent(this, AddActivity::class.java)
             intent.putExtra(AddActivity.TASK_ID, it.id)
-            startActivityForResult(intent, CREATE_NEW_TASK)
+            register.launch(intent)
         }
 
         adapter.listenerDelete = {
@@ -41,26 +54,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK){
-            binding.rvTasks.adapter = adapter
-            adapter.submitList(TaskDataSource.getList())
-        }
-            updateList()
-
-    }
-
     private fun updateList(){
-        val  list = TaskDataSource.getList()
+        val list = TaskDataSource.getList()
         binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
          else View.GONE
 
         adapter.submitList(list)
-    }
-
-    companion object {
-        private const val CREATE_NEW_TASK = 1000
     }
 
 }
